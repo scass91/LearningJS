6e1ae1ca55c86dd3cccfc4efc1f7c8bc042af91d
Simon Cass <scass1991@gmail.com>
2019-01-15 17:21:35 +0000

    Passing buzz test

--- FizzBuzzJS/jasmine/src/fizzBuzz.js
+++ FizzBuzzJS/jasmine/src/fizzBuzz.js
@@ -1,9 +1,10 @@
 function fizzBuzz(number) {
 if(number % 3 === 0) {
   return "fizz"
-} else {
+} else if (number % 5 === 0) {
+  return "buzz"
+} else
   return number
-}
 }
 
 for(i=1;i<101;i++) {
