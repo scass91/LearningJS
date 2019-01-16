dcdabbec5bb5e405a96d5b334f4256b290b6fe24
Simon Cass <scass1991@gmail.com>
2019-01-16 10:30:41 +0000

    Grouped tests together

--- FizzBuzzJS/jasmine/spec/fizzBuzzSpec.js
+++ FizzBuzzJS/jasmine/spec/fizzBuzzSpec.js
@@ -1,5 +1,5 @@
 describe("fizzBuzz", function() {
-
+  describe("Specific Numbers", function() {
     it("2 for 2", function() {
       expect(fizzBuzz(2)).toEqual(2)
     })
@@ -12,16 +12,37 @@ describe("fizzBuzz", function() {
       expect(fizzBuzz(13)).toEqual(13)
     })
 
-    it("Return fizz for numbers divisible by 3", function() {
+  })
+
+  describe("Multiples of 3", function() {
+    it("Return fizz for 3", function() {
       expect(fizzBuzz(3)).toEqual("fizz")
     })
 
-    it("Return buzz for numbers divisible by 5", function() {
+    it("Return fizz for 6", function() {
+      expect(fizzBuzz(6)).toEqual("fizz")
+    })
+
+  })
+
+  describe("Multiples of 5", function() {
+    it("Return buzz for 5", function() {
       expect(fizzBuzz(5)).toEqual("buzz")
     })
 
-    it("Return fizzbuzz for numbers divisible by 15", function() {
+    it("Return buzz for 10", function() {
+      expect(fizzBuzz(10)).toEqual("buzz")
+    })
+
+  })
+
+  describe("Multiples of 15", function() {
+    it("Return fizzbuzz for 15", function() {
       expect(fizzBuzz(15)).toEqual("fizzbuzz")
     })
 
+    it("Return fizzbuzz for 30", function() {
+      expect(fizzBuzz(30)).toEqual("fizzbuzz")
+    })
+  })
 })
